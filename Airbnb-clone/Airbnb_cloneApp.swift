c4818d83a611ee44cfe36ade81fdba9e542783fa
ymnberkay <berkay.yaman@hotmail.com>
2024-08-30 15:37:33 +0300

    First commit

---
+++ Airbnb-clone/Airbnb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_cloneApp.swift
+//  Airbnb-clone
+//
+//  Created by Berkay Yaman on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb-clone
+//
+//  Created by Berkay Yaman on 30.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
